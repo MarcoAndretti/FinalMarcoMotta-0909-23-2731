33e5e82eea918b0f4ee6fbd343dc1335ebbb0463
MarcoAndretti <mrodriguezm32@miumg.edu.gt>
2024-06-15 08:51:03 -0600

    Metodos

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,44 @@ public class Main {
         this.scanner = new Scanner(System.in);
     }
 
+    public void mostrarMayorDeTres() {
+        System.out.print("Ingrese el primer número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Ingrese el tercer número: ");
+        int num3 = scanner.nextInt();
+
+        int mayor = num1;
+        if (num2 > mayor) {
+            mayor = num2;
+        }
+        if (num3 > mayor) {
+            mayor = num3;
+        }
+        System.out.println("El número mayor es: " + mayor);
+    }
+
+    public void mostrarTablaMultiplicar() {
+        System.out.print("Ingrese un número: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+
+    public void sumarParesHasta() {
+        System.out.print("Ingrese un número: ");
+        int n = scanner.nextInt();
+        int suma = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                suma += i;
+            }
+        }
+        System.out.println("La suma de los números pares es: " + suma);
+    }
+
 
     public static void main(String[] args) {
         Main ejercicios = new Main();
